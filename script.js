b6c1c39410f4d8da182811ae89aa938524454bd2
soumya3969 <srmhnt2001@gmail.com>
2024-12-15 15:57:49 +0530

    traversing in array

---
+++ script.js
@@ -0,0 +1,9 @@
+const arr=[8,2,5,2,45,-5,6]
+const arr2=[8,2,5,2,45,-5,6]
+// sort the array
+let sorted=arr.sort((a,b)=>a-b)
+// console.log(sorted)
+
+
+console.log("---------Traversal using for loop---------");
+
